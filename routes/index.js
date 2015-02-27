2e42ca39645104765d51669d402f36823f3dc2db
Rain Ramm <rammrain@gmail.com>
2015-02-27 16:25:32 +0200

    Remove logging.

--- routes/index.js
+++ routes/index.js
@@ -17,8 +17,6 @@ router.get('/temp', function(req, res) {
 
 /* POST temperature */
 router.post('/temp', function(req, res) {
-    console.log(req.body);
-
     saveTemp(req.body.temp, req.body.time);
 
     res.json({
