991a0b939642e8f2bd094dd52338136a8a7f11a5
Rain Ramm <rammrain@gmail.com>
2015-02-27 15:34:23 +0200

    Remove logger.

--- app.js
+++ app.js
@@ -1,25 +1,13 @@
 var express      = require('express'),
     path         = require('path'),
-    favicon      = require('serve-favicon'),
-    logger       = require('morgan'),
     bodyParser   = require('body-parser'),
     routes       = require('./routes/index'),
     app          = express();
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
 
 module.exports = app;
